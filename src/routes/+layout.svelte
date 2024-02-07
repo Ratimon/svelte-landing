32db8f1fdd86aa47205d02a58c65d6631972bfb1
Ratimon <ratixoxo@gmail.com>
2024-02-07 11:01:10 +0700

    Feat (./src/lib/components): Adding Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,50 @@
 <script>
-    import '../app.postcss';
-  </script>
+	import '../app.postcss';
+    import NavTabs from '$lib/components/daisyui/NavTabs.svelte';
+    import {name, description, themeColor, appleStatusBarStyle} from 'web-config';
+    import {url} from '$lib/utils/path';
+
+</script>
+
+<svelte:head>
+	<title>{name}</title>
+	<meta name="title" content={name} />
+	<meta name="description" content={description} />
+
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={name} />
+	<meta property="og:description" content={description} />
+	<meta property="twitter:title" content={name} />
+	<meta property="twitter:description" content={description} />
+
+	<!-- minimal -->
+	<!-- TODO automatise -->
+  <!-- TODO fix pwa -->
+	<!-- <link rel="icon" href={url('/pwa/favicon.svg')} type="image/svg+xml" />
+	<link rel="icon" href={url('/pwa/favicon.ico')} sizes="any" />32×32
+	<link rel="apple-touch-icon" href={url('/pwa/apple-touch-icon.png')} />
+  <link rel="manifest" href={url('/pwa/manifest.webmanifest')} /> -->
+
+	<!-- extra info -->
+	<meta name="theme-color" content={themeColor} />
+	<meta name="mobile-web-app-capable" content="yes" />
+	<meta name="application-name" content={name} />
+
+	<!-- apple -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content={appleStatusBarStyle} />
+	<meta name="apple-mobile-web-app-title" content={name} />
+</svelte:head>
+
+<div class="relative top-0 z-50 navbar bg-base-100 h-16 p-1 border-b-2 border-primary ">
+	<div class="flex-1 place-content-center" >
+		<NavTabs
+			pages={[
+				{pathname: '/', title: 'Home'},
+				// {pathname: '/', title: 'Home'},
+			]}
+		/>
+	</div>
+</div>
   
-  <slot />
+<slot />
