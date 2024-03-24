954d8b45ba123214a58c35d2c4fa875e16b2014c
Ratimon <ratixoxo@gmail.com>
2024-03-24 11:56:32 +0700

    Chore (./): Removing console

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -6,18 +6,13 @@ async function getPosts() {
 
 	const paths = import.meta.glob('/src/posts/*.md', { eager: true })
 
-	console.log("paths", paths)
-
 	for (const path in paths) {
 		const file = paths[path]
 		const slug = path.split('/').at(-1)?.replace('.md', '')
 
-		console.log("slug", slug)
-
 		if (file && typeof file === 'object' && 'metadata' in file && slug) {
 			const metadata = file.metadata as Omit<Post, 'slug'>
 			const post = { ...metadata, slug } satisfies Post
-			console.log("post", post)
 			post.published && posts.push(post)
 		}
 	}
@@ -25,9 +20,6 @@ async function getPosts() {
 	posts = posts.sort((first, second) =>
     new Date(second.date).getTime() - new Date(first.date).getTime()
 	)
-
-	console.log("posts", posts)
-
 
 	return posts
 }
